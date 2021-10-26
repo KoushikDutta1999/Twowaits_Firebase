0a2877d61176134b6e424e8fca8685b5c08ed54e
Koushik Dutta <koushikdutta.bca2019@nsec.ac.in>
2021-10-26 17:56:55 +0530

    first commit

---
+++ Twowaits_Firebase/assets/js/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoxSjfU8eR63hvNKPFdP4-3RiNre0PZt0",
+  authDomain: "twowaitsproject.firebaseapp.com",
+  projectId: "twowaitsproject",
+  storageBucket: "twowaitsproject.appspot.com",
+  messagingSenderId: "475738224762",
+  appId: "1:475738224762:web:18b4096f80e0deaeefe3c3"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
